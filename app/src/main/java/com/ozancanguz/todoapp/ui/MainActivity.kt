9ce24099fe54c52127ef16b08d09f95944eb4ad0
Ozan <ozancangu24@gmail.com>
2022-12-10 21:20:19 +0300

    -fragment transitions created

--- app/src/main/java/com/ozancanguz/todoapp/ui/MainActivity.kt
+++ app/src/main/java/com/ozancanguz/todoapp/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.ozancanguz.todoapp
+package com.ozancanguz.todoapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.ozancanguz.todoapp.R
 
 
 class MainActivity : AppCompatActivity() {
