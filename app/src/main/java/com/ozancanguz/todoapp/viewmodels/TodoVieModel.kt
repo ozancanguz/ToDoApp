993a61b04a3a13ba5049acf8e38a534b9c957c4e
Ozan <ozancangu24@gmail.com>
2022-12-10 18:32:37 +0300

    -to do viewmodel created and get all references

---
+++ app/src/main/java/com/ozancanguz/todoapp/viewmodels/TodoVieModel.kt
@@ -0,0 +1,39 @@
+package com.ozancanguz.todoapp.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.ozancanguz.todoapp.data.db.ToDoDatabase
+import com.ozancanguz.todoapp.data.model.ToDo
+import com.ozancanguz.todoapp.data.repository.Repository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TodoVieModel(application: Application,repository: Repository):AndroidViewModel(application) {
+
+    // get dao ref
+    private val toDoDao=ToDoDatabase.getDatabase(application).todoDao()
+
+    //get repository ref
+    private val repository = Repository(toDoDao)
+
+    //get all data list
+    val getAllData: LiveData<List<ToDo>>
+
+    // init get all data
+    init {
+        getAllData=repository.getAllData
+    }
+
+     // inserting data with background thread
+    fun insertData(toDo: ToDo){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.insertData(toDo)
+        }
+    }
+
+
+
+
+}
