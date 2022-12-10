42c3bcbac617a8f38aa8b385f24042d84673b564
Ozan <ozancangu24@gmail.com>
2022-12-10 19:06:00 +0300

    -list fragment adapter created

---
+++ app/src/main/java/com/ozancanguz/todoapp/adapter/ListAdapter.kt
@@ -0,0 +1,51 @@
+package com.ozancanguz.todoapp.adapter
+
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ozancanguz.todoapp.R
+import com.ozancanguz.todoapp.data.model.ToDo
+import kotlinx.android.synthetic.main.list_row_layout.view.*
+
+class ListAdapter:RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+
+    var todolist= emptyList<ToDo>()
+
+
+    // update ui
+     fun updateData(newData:List<ToDo>){
+         this.todolist=newData
+     }
+
+    class MyViewHolder(view: View):RecyclerView.ViewHolder(view){
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val inflater=LayoutInflater.from(parent.context)
+        val view=inflater.inflate(R.layout.list_row_layout,parent,false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val currentItem=todolist[position]
+        holder.itemView.title_txt.text=currentItem.title
+        holder.itemView.description_txt.text=currentItem.description
+
+
+
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return todolist.size
+    }
+
+
+}
